3c68a84cfad01383222c7bb4d4319f10de6fe60d
Kritisingh09 <kritisingh917.ks@gmail.com>
2024-09-09 19:56:10 +0530

    Episode 3 completed

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-//React Element -Core React
-// const heading=React.createElement("h1",{id:"heading"},"Hello Episode 3");
-// console.log("heading",heading);
-//JSX
 //React element using JSX
-const jsxHeading=(<h1 id="heading" className="heading" tabIndex="1">
-    Namaste React using JSX</h1>)
-console.log("jsxHeading",jsxHeading);
+//React element
+// const heading=(<h1 id="heading" className="heading" tabIndex="1">
+//     Namaste React using JSX</h1>)
+// //React component-functional component
+
+const Title=()=>(
+<h3 className="title">Practice React
+
+</h3>
+
+)
+//React Element or jsx
+const subTitle=(
+    <p>Here Akshay Saini is Teaching React</p>
+)
+const elem=(
+    <span>It is a React Element</span>
+)
+//<Title/> and <Title></Title> are same 
+const HeadingComponent=()=> (
+    <div id="container">
+        <Title/> 
+        <Title></Title>
+        {Title()}
+        
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading)
+// root.render(heading)
+root.render(<HeadingComponent/>) // this is how babel understands to render a component
